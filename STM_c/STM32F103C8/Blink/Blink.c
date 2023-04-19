1d21435a7635b6086684ff1f6bd906cee2567a14
ogneyar <ya13th@mail.ru>
2023-04-19 09:01:59 +0300

    goood

--- STM_c/STM32F103C8/Blink/Blink.c
+++ STM_c/STM32F103C8/Blink/Blink.c
@@ -1,26 +1,29 @@
 //
 #include <stdint.h>
+// #include "stm32f10x.h"
  
 #define APB2ENR   (*(uint32_t*)(0x40021000+0x18))
 #define APB2ENR_ENIOC (1u<<4u)
  
 #define GPIOC_CRH (*(uint32_t*)(0x40011000+0x04))
-#define xGPIO_CRH_MODE13_0 (1u << 20u)
-#define xGPIO_CRH_MODE13_1 (1u << 21u)
-#define xGPIO_CRH_CNF13 (3u << 22u) 
+#define GPIO_CRH_MODE13_0 (1u << 20u)
+#define GPIO_CRH_MODE13_1 (1u << 21u)
+#define GPIO_CRH_CNF13 (3u << 22u) 
  
 #define GPIOC_ODR (*(uint32_t*)(0x40011000+0x0c))
 #define GPIO_ODR_PIN_13 (1u << 13u)
- 
-uint16_t delay_ms = 10;
+
+void delay(uint16_t inter);
+
+uint16_t delay_ms = 100;
 
 __attribute__((noreturn))
 void Reset_Handler(){
     //Разрешаем тактировать GPIOC на шине APB2
     APB2ENR |= APB2ENR_ENIOC;
     // Настраиваем GPIO Pin 13 как выход Push-Pull на максимальной частоте
-    // GPIOC_CRH &= ~xGPIO_CRH_CNF13;
-    GPIOC_CRH |= xGPIO_CRH_MODE13_0 | xGPIO_CRH_MODE13_1;
+    GPIOC_CRH &= ~GPIO_CRH_CNF13;
+    GPIOC_CRH |= GPIO_CRH_MODE13_0 | GPIO_CRH_MODE13_1;
     while(1){
         // Переключаем пин 13 на порте C
         GPIOC_ODR ^= GPIO_ODR_PIN_13;
@@ -50,5 +53,5 @@ const ISR_VECTOR_t  isr_vector = {
 
 // delay
 void delay(uint16_t inter) {
-	for(uint32_t i = 0; i < (uint32_t)(inter<<12); i++) __asm("nop");
+	for(uint32_t i = 0; i < (uint32_t)(inter<<9); i++) __asm("nop");
 }
--- STM_c/STM32F103C8/Blink_NL/Blink_NL.c
+++ STM_c/STM32F103C8/Blink_NL/Blink_NL.c
@@ -1,5 +1,6 @@
 //
 #include "Blink_NL.h"
+// #include "stm32f10x.h"
 
 #define RCC_APB2ENR   (*(uint32_t*)(0x40021000+0x18))
 #define RCC_APB2ENR_IOPCEN (1u<<4u)
@@ -7,6 +8,7 @@
 #define GPIOC_CRH (*(uint32_t*)(0x40011000+0x04))
 #define GPIO_CRH_MODE13_0 (1u <<20u)
 #define GPIO_CRH_MODE13_1 (1u <<21u)
+#define GPIO_CRH_CNF13 ((uint32_t)0x00C00000)
  
 #define GPIOC_ODR (*(uint32_t*)(0x40011000+0x0c))
 #define GPIO_ODR_ODR13 (1u << 13u)
@@ -16,12 +18,13 @@ int main(void) {
     //Разрешаем тактировать GPIOC на шине APB2
     RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
     // Настраиваем GPIO Pin 13 как выход Push-Pull на максимальной частоте
+    GPIOC_CRH &= ~GPIO_CRH_CNF13;
     GPIOC_CRH |= GPIO_CRH_MODE13_0 | GPIO_CRH_MODE13_1;
     // while(1){
 loop_main:
         // Переключаем пин 13 на порте C
         GPIOC_ODR ^= GPIO_ODR_ODR13;
-        delay(50);
+        delay(100);
     // }
     goto loop_main;
 }
@@ -29,5 +32,5 @@ loop_main:
 
 // delay
 void delay(uint16_t inter) {
-	for(uint32_t i = 0; i < (uint32_t)(inter<<12); i++) __asm("nop");
+	for(uint32_t i = 0; i < (uint32_t)(inter<<9); i++) __asm("nop");
 }
