1fe9636bf71c63750eeb205eceaac2b9208db0fe
ogneyar <ya13th@mail.ru>
2023-08-30 15:02:35 +0300

    auto commit

---
+++ MDR_c/MDR32F9Q2I/USART/main.h
@@ -0,0 +1,29 @@
+//
+#include "MDR32F9Q2I.h"
+#include <stdint.h>
+
+extern int main(void);
+
+__attribute__((noreturn))
+void Reset_Handler(){
+    main();
+}
+ 
+// Объявим тип - указатель на прерывание
+typedef void (*isr_routine)(void);
+ 
+// Опишем структуру таблицы векторов прерываний
+typedef struct  {
+    const uint32_t * stack_top;
+    const isr_routine reset;
+} ISR_VECTOR_t;
+ 
+//Получим адрес указателья на стек из скрипта линковки
+extern const uint32_t __StackTop;
+ 
+//Укажем линковщику, что эту константу нужно положить в секцию .isr_vector
+__attribute__((section(".isr_vector"), __unused__))
+const ISR_VECTOR_t  isr_vector = {
+    .stack_top = &__StackTop,
+    .reset  = &Reset_Handler,
+};
